21d4520826c8caf3e278bbe47c0ff8aa1f119557
Michael Yuan <michael@michaelyuan.com>
2019-12-15 02:12:52 -0600

    Update demo.js

--- code/chapter04/demo.js
+++ code/chapter04/demo.js
@@ -1,3 +1,9 @@
+/* Don't modify */
+var abi = [];
+var bytecode = '';
+var cAddr = '';
+/* Don't modify */
+
 var instance = null;
 window.addEventListener('web3Ready', function() {
   var contract = web3.ss.contract(abi);
